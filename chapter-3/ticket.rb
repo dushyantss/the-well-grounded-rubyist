b7a64ae132bba8a8206c251a9f076065f56b8a85
Dushyant Singh Shekhawat <contact@dushyantss.com>
2019-06-02 20:12:11 +0530

    Add a dummy ticket.rb

---
+++ chapter-3/ticket.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Ticket
+    def initialize(venue, date, price)
+        @venue = venue
+        @date = date
+        @price = price
+     
+  end
+    # etc.
+
+    def price
+        @price
+     
+  end
+    # etc.
+end
